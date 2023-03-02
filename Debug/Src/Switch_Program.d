5c330559ee4ba3f265e0019eb5841bc9e3ba36dc
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2023-03-02 21:09:10 +0200

    Delete Debug directory

--- Debug/Src/Switch_Program.d
+++
@@ -1,9 +0,0 @@
-Src/Switch_Program.o: ../Src/Switch_Program.c ../Inc/STD_Types.h \
- ../Inc/BitOperations.h ../Inc/GPIO_Interface.h ../Inc/Switch_Config.h \
- ../Inc/Switch_Private.h ../Inc/Switch_Interface.h
-../Inc/STD_Types.h:
-../Inc/BitOperations.h:
-../Inc/GPIO_Interface.h:
-../Inc/Switch_Config.h:
-../Inc/Switch_Private.h:
-../Inc/Switch_Interface.h:
