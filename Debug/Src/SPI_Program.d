d5708d52d861b246c90f40e83ba444722369ce39
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2023-04-08 22:19:13 +0200

    Delete Debug directory

--- Debug/Src/SPI_Program.d
+++
@@ -1,8 +0,0 @@
-Src/SPI_Program.o: ../Src/SPI_Program.c ../Inc/STD_Types.h \
- ../Inc/BitOperations.h ../Inc/SPI_Config.h ../Inc/SPI_Private.h \
- ../Inc/SPI_Interface.h
-../Inc/STD_Types.h:
-../Inc/BitOperations.h:
-../Inc/SPI_Config.h:
-../Inc/SPI_Private.h:
-../Inc/SPI_Interface.h:
--- Debug/Src/Stepper_Program.d
+++
@@ -1,5 +0,0 @@
-Src/Stepper_Program.o: ../Src/Stepper_Program.c ../Inc/STD_Types.h \
- ../Inc/BitOperations.h ../Inc/GPIO_Interface.h
-../Inc/STD_Types.h:
-../Inc/BitOperations.h:
-../Inc/GPIO_Interface.h:
