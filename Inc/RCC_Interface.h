a82e69316853180c1a89861e6f3a506f012406bc
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2023-04-24 22:52:49 +0200

    Update RCC_Interface.h

--- Inc/RCC_Interface.h
+++ Inc/RCC_Interface.h
@@ -19,12 +19,12 @@
 u8 RCC_u8SystemClkInitialization (void);
 
 /****************************************************/
-/* Define											*/
-/*		1) RCC Bus									*/
-/* 				- AHB1								*/
-/* 				- AHB2								*/
-/* 				- APB1								*/
-/* 				- APB2								*/
+/* Define                                           */
+/*    1) RCC Bus                                    */
+/*        - AHB1                                    */
+/*        - AHB2                                    */
+/*        - APB1                                    */
+/*        - APB2                                    */
 /****************************************************/
 #define	RCC_AHB1	1
 #define	RCC_AHB2	2
@@ -32,42 +32,42 @@ u8 RCC_u8SystemClkInitialization (void);
 #define	RCC_APB2	4
 
 /************************************************************************************/
-/* Define 																			*/
-/*		1) RCC AHB1 peripheral clock enable\disable register						*/
-/*			  *** PORTS Enable\Disable ***											*/
-/* 				GPIOA --> Perphiral = 0												*/
-/* 				GPIOB --> Perphiral = 1												*/
-/* 				GPIOC --> Perphiral = 2												*/
-/*																					*/
-/*		2) RCC AHB2 peripheral clock enable\disable register						*/
-/* 				OTGFS --> Perphiral = 7												*/
-/*																					*/
-/*		3) RCC APB1 peripheral clock enable\disable register						*/
-/* 				TIM2 --> Perphiral = 0												*/
-/* 				TIM3 --> Perphiral = 1												*/
-/* 				TIM4 --> Perphiral = 2												*/
-/* 				TIM5 --> Perphiral = 3												*/
-/* 				WWDG --> Perphiral = 11												*/
-/* 				SPI2 --> Perphiral = 14												*/
-/* 				SPI3 --> Perphiral = 15												*/
-/* 				USART2 --> Perphiral = 17											*/
-/* 				I2C1 --> Perphiral = 21												*/
-/* 				I2C2 --> Perphiral = 22												*/
-/* 				I2C3 --> Perphiral = 23												*/
-/* 				PWR --> Perphiral = 28												*/
-/*																					*/
-/*		4) RCC APB2 peripheral clock enable\disable register						*/
-/* 				TIM1 --> Perphiral = 0												*/
-/* 				USART1 --> Perphiral = 4											*/
-/* 				USART6 --> Perphiral = 5											*/
-/* 				ADC1 --> Perphiral = 8												*/
-/* 				SDIO --> Perphiral = 11												*/
-/* 				SPI1 --> Perphiral = 12												*/
-/* 				SPI4 --> Perphiral = 13												*/
-/* 				SYSCFG --> Perphiral = 14 (EXTI)									*/
-/* 				TIM9 --> Perphiral = 16												*/
-/* 				TIM10 --> Perphiral = 17											*/
-/* 				TIM11 --> Perphiral = 18											*/
+/* Define                                                                           */
+/*    1) RCC AHB1 peripheral clock enable\disable register                          */
+/*        *** PORTS Enable\Disable ***                                              */
+/*        GPIOA --> Perphiral = 0                                                   */
+/*        GPIOB --> Perphiral = 1                                                   */
+/*        GPIOC --> Perphiral = 2                                                   */
+/*                                                                                  */
+/*    2) RCC AHB2 peripheral clock enable\disable register                          */
+/*        OTGFS --> Perphiral = 7                                                   */
+/*                                                                                  */
+/*    3) RCC APB1 peripheral clock enable\disable register                          */
+/*        TIM2 --> Perphiral = 0                                                    */
+/*        TIM3 --> Perphiral = 1                                                    */
+/*        TIM4 --> Perphiral = 2                                                    */
+/*        TIM5 --> Perphiral = 3                                                    */
+/*        WWDG --> Perphiral = 1                                                    */
+/*        SPI2 --> Perphiral = 1                                                    */
+/*        SPI3 --> Perphiral = 1                                                    */
+/*        USART2 --> Perphiral = 17                                                 */
+/*        I2C1 --> Perphiral = 21                                                   */
+/*        I2C2 --> Perphiral = 22                                                   */
+/*        I2C3 --> Perphiral = 23                                                   */
+/*        PWR --> Perphiral = 28                                                    */
+/*                                                                                  */
+/*    4) RCC APB2 peripheral clock enable\disable register                          */
+/*        TIM1 --> Perphiral = 0                                                    */
+/*        USART1 --> Perphiral = 4                                                  */
+/*        USART6 --> Perphiral = 5                                                  */
+/*        ADC1 --> Perphiral = 8                                                    */
+/*        SDIO --> Perphiral = 11                                                   */
+/*        SPI1 --> Perphiral = 12                                                   */
+/*        SPI4 --> Perphiral = 13                                                   */
+/*        SYSCFG --> Perphiral = 14 (EXTI)                                          */
+/*        TIM9 --> Perphiral = 16                                                   */
+/*        TIM10 --> Perphiral = 17                                                  */
+/*        TIM11 --> Perphiral = 18                                                  */
 /************************************************************************************/
 #define RCC_GPIOA	 0
 #define RCC_GPIOB	 1
