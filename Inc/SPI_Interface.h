7710308efaccc6d080fbc5070bb69cf95ba29e80
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2023-04-24 22:55:57 +0200

    Update SPI_Interface.h

--- Inc/SPI_Interface.h
+++ Inc/SPI_Interface.h
@@ -11,53 +11,53 @@
 #define SPI_INTERFACE_H_
 
 /** Error States **/
-#define SPI_ErrorClkPhase					13
-#define SPI_ErrorClkPolarity				14
-#define SPI_ErrorBaudRate					15
-#define SPI_ErrorFormatTrans				16
+#define SPI_ErrorClkPhase			13
+#define SPI_ErrorClkPolarity			14
+#define SPI_ErrorBaudRate			15
+#define SPI_ErrorFormatTrans			16
 #define SPI_ErrorSlaveSoftwareSelection		17
-#define SPI_ErrorDataFormat					18
-#define SPI_ErrorEnable						19
+#define SPI_ErrorDataFormat			18
+#define SPI_ErrorEnable				19
 
 typedef struct
 {
 	u32	CR1;		// SPI control register 1 (not used in I2S mode)
 	u32	CR2;		// SPI control register 2
-	u32	SR;			// SPI status register
-	u32	DR;			// SPI data register
+	u32	SR;		// SPI status register
+	u32	DR;		// SPI data register
 	u32	CRCPR;		// SPI CRC polynomial register (not used in I2S mode)
 	u32	RXCRCR;		// SPI RX CRC register (not used in I2S mode)
 	u32 TXCRCR;		// SPI TX CRC register (not used in I2S mode)
-	u32 I2SCFGR;	// SPI_I2S configuration register
+	u32 I2SCFGR;		// SPI_I2S configuration register
 	u32 I2SPR;		// SPI_I2S PreScaler register
 }SPI_MemoryMap;
 
 #define	SPI1		( (volatile SPI_MemoryMap *)(0x40013000) )
 #define	SPI2		( (volatile SPI_MemoryMap *)(0x40003800) )
 #define	SPI3		( (volatile SPI_MemoryMap *)(0x40003C00) )
-/****************************************************/
-/* Define Which SPI you will Use:					*/
-/*          -> SPI1									*/
-/*          -> SPI2									*/
-/*          -> SPI6									*/
-/****************************************************/
+/************************************************/
+/* Define Which SPI you will Use:		*/
+/*          -> SPI1				*/
+/*          -> SPI2				*/
+/*          -> SPI6				*/
+/************************************************/
 
-/************************************************************/
-/* FIRST: Define: Master Mode								*/
-/*															*/
-/* OR: Define: Slave Mode									*/
-/*			1- En/Dis able Slave SoftWare Selection			*/
-/*			2- Select/UnSelect The Slave					*/
-/* THEN: Initialize:										*/
-/*		1- Clock Phase										*/
-/*		2- Clock Polarity									*/
-/*		3- Clock BaudRate									*/
-/*		4- Frame Format										*/
-/*		5- Data Format										*/
-/*		6- Enable or Disable SPI							*/
-/*															*/
-/*															*/
-/************************************************************/
+/************************************************************************/
+/* FIRST: Define: Master Mode						*/
+/*									*/
+/* OR: Define: Slave Mode						*/
+/*			1- En/Dis able Slave SoftWare Selection		*/
+/*			2- Select/UnSelect The Slave			*/
+/* THEN: Initialize:							*/
+/*		1- Clock Phase						*/
+/*		2- Clock Polarity					*/
+/*		3- Clock BaudRate					*/
+/*		4- Frame Format						*/
+/*		5- Data Format						*/
+/*		6- Enable or Disable SPI				*/
+/*									*/
+/*									*/
+/************************************************************************/
 
 #define SPI_LeadingEdge    0
 #define SPI_TrailingEdge   1
