f650a0cb2dbea9f97ac6e062bd3ab58818c2508c
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2023-12-03 20:55:24 +0200

    Add files via upload

--- Src/Link_Porgram.c
+++ Src/Link_Porgram.c
@@ -33,9 +33,9 @@ u8 Link_u8ReturnQueueSize()
 
 void Link_voidInitQueue()
 {
-    for (u8 i =0; i<MAX_QUEUE_SIZE; i++)
+    for (u8 i =0; i < MAX_QUEUE_SIZE; i++)
     {
-        Private_Queue.Arr[i]=0xFF;
+        Private_Queue.Arr[i]= 0xFF;
     }
     Private_Queue.head      = 0;
     Private_Queue.tail      = 0;
@@ -45,22 +45,22 @@ void Link_voidInitQueue()
 void Link_voidEnqueueMessage(LinkMessage_t Copy_LinkMessage)
 {
     
-    if (Private_Queue.head == Private_Queue.tail || Private_Queue.elemNum==0)
+    if (Private_Queue.head == Private_Queue.tail || Private_Queue.elemNum == 0)
     {
         Private_Queue.Arr[Private_Queue.tail]= 
             (
-            ((u16)(((Copy_LinkMessage.source << 4) | Copy_LinkMessage.type)<<8))
+            ((u16)(((Copy_LinkMessage.source << 4) | Copy_LinkMessage.type) << 8))
             |
             ((u16)Copy_LinkMessage.data)
             );
         Private_Queue.tail++;
         Private_Queue.elemNum++;
     }
-    else if (Private_Queue.elemNum<MAX_QUEUE_SIZE)
+    else if (Private_Queue.elemNum < MAX_QUEUE_SIZE)
     {
         Private_Queue.Arr[Private_Queue.tail]= 
             (
-            ((u16)(((Copy_LinkMessage.source << 4) | Copy_LinkMessage.type)<<8)) |
+            ((u16)(((Copy_LinkMessage.source << 4) | Copy_LinkMessage.type)<< 8)) |
             ((u16)Copy_LinkMessage.data)
             );
         Private_Queue.tail=(Private_Queue.tail+1)%16;
@@ -71,7 +71,7 @@ void Link_voidEnqueueMessage(LinkMessage_t Copy_LinkMessage)
 u8 Link_u8SendDequeueMessage(volatile u16 *P_u16ReturnVal)
 {
 	//  returns a copy of the head element.
-    if (Private_Queue.elemNum>0)
+    if (Private_Queue.elemNum > 0)
     {
         *P_u16ReturnVal = Private_Queue.Arr[Private_Queue.head];
         Private_Queue.head=(Private_Queue.head+1)%16;
@@ -89,12 +89,13 @@ void Link_voidInit()
 
 u8 Link_u8SendMessage()
 {
-    if (Private_Queue.elemNum==0)
+    if (Private_Queue.elemNum == 0)
         return 1;
 
-    if (Private_Queue.elemNum>=16 )
+    if (Private_Queue.elemNum >= 16 )
     	GPIO_u8FastControlPinValue(GPIO_PORTC, GPIO_PIN13, GPIO_LOW);
-    while (Private_Queue.elemNum!=0)
+    
+	while (Private_Queue.elemNum != 0)
     {
         // Hold the system until SPI is ready to send message.
         while (GET_BIT(LINK_RASPBERRY_PI->SR,TXE) == 0 && GET_BIT(LINK_RASPBERRY_PI->SR,BSY) == 1);
